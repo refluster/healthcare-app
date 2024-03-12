0c820b5a4b509b8d5123cb6e932b6aaf62ffa395
Koh Uehara <refluster@gmail.com>
2024-03-11 17:20:45 -0700

    update with post and feedback

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -28,34 +28,6 @@ const IndexPage: React.FC = () => {
         })();
     }, [user, navigate]);
 
-    const data = [
-        {
-            title: "戦闘力を高める事故投資",
-            description: "そらっちの銀行トランザクションの事例、事故資産の説明、など。",
-        }, {
-            title: "書籍がもつ知的好奇心の効果",
-            description: "めちゃコミの書籍が与える知的な健康への影響。",
-        }, {
-            title: "過度な攻撃性の件",
-            description: "発信内容の少しの気遣いから授かる中期的なポジティブインパクト。",
-        }, {
-            title: "衝動買いに対する意識",
-            description: "衝動買いは即時の感情的満足を追求する行動であり、長期的な後悔や経済的不安を引き起こすことがあります。計画的な購入と比較し、心理的な不満を一時的に解消することが目的であることが研究で明らかにされています（Verplanken & Sato, 2011年）。",
-        }, {
-            title: "時間管理と優先順位付け",
-            description: "効果的な時間管理は、生産性の向上、ストレスの軽減、そしてより満足のいく生活へと繋がります。目標設定理論に基づき、SMART（具体的、測定可能、達成可能、現実的、時間的に限定された）目標を設定することが推奨されています（Locke & Latham, 2002年）。",
-        }, {
-            title: "感謝の習慣",
-            description: "感謝は幸福感を高める重要な要素であり、感謝日記をつけることなどにより、人々の幸福感、健康、睡眠の質が向上することが示されています（Emmons & McCullough, 2003年）。",
-        }, {
-            title: "ストレス管理",
-            description: 'ストレス管理の技術、特に定期的な運動や瞑想は、ストレスホルモンのレベルを下げ、心理的なウェルビーイングを高めることが知られています（Pedersen & Saltin, 2015年；Goyal et al., 2014年）。',
-        }, {
-            title: '健康的な生活習慣',
-            desctiption: '健康的な食事、運動、十分な睡眠は、全体的な幸福感と生産性に直接的な影響を与えます。これらは心身の健康を維持し、慢性疾患のリスクを低減します（Harris, 2009年；Chaput, 2014年）。'
-        }
-    ];
-
     const doPost = async (text: string) => {
         const baseUrl = 'https://cykubbplcd.execute-api.us-west-2.amazonaws.com/Prod';
         setInput(text);
