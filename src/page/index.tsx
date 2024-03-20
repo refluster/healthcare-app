d874abde2d64d3922ff267377b267a287b8b412c
Koh Uehara <refluster@gmail.com>
2024-03-20 14:05:52 -0700

    unified message model

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -6,9 +6,12 @@ import { auth } from '../lib/firebase';
 import { useNavigate } from "react-router-dom";
 import PostButton from '../component/post-button';
 import axios from 'axios';
+import * as api from '../api';
 
 type Message = {
-    title: string;
+    author: 'user' | 'system';
+    userId: string;
+    title?: string;
     content: string;
 }
 
@@ -23,13 +26,20 @@ const IndexPage: React.FC = () => {
             console.log(user);
             if (!user) {
                 navigate('/intro');
+                return;
             }
+            //api.getJournals(user.uid);
         })();
     }, [user, navigate]);
 
     const doPost = async (text: string) => {
         const baseUrl = 'https://cykubbplcd.execute-api.us-west-2.amazonaws.com/Prod';
-        setInput(text);
+        //setInput(text);
+        setMessages([{
+            author: 'user',
+            userId: user!.uid,
+            content: text,
+        }])
         try {
             const response = await axios.post(`${baseUrl}/healthcare-gpt/generic`, {
                 message: text + 'この行動や情動に対して、お金の使い方を、自らのために投資する、支払いは消費ではなく投資である意味づけ、そんな解釈について、3つフィードバックしてください。',
@@ -43,17 +53,35 @@ const IndexPage: React.FC = () => {
                 type: 'expert',
             });
             const msg2 = response2.data.wellness_expert_advice.advice;
-            setMessages([...msg, ...msg2]);
+            setMessages(prev => {
+                return [...prev, ...msg, ...msg2]
+            });
+            console.log(text, [...msg, ...msg2])
+            const input = [
+                {
+                    userId: user?.uid,
+                    author: 'user',
+                    content: text,
+                },
+                ...[...msg, ...msg2].map(data => ({
+                    ...data,
+                    userId: user?.uid,
+                    author: 'system',
+                }))
+            ];
+            console.log(input);
+            api.createJournals(input);
         } catch (error) {
             console.error('API call failed:', error);
         }
     };
-    
+
     return (
         <Box sx={{ position: "relative" }}>
             <Header title="Wellness" />
             <Box sx={{ pt: 15 }}>
                 <Typography variant='h1' style={{ marginLeft: 16 }}>Home</Typography>
+                {/*
                 <Card sx={{ my: 2 }}>
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
@@ -71,21 +99,38 @@ const IndexPage: React.FC = () => {
                         </Typography>
                     </CardContent>
                 </Card>
+                        */}
                 {
-                    messages.map(d => (<Card sx={{ my: 2 }}>
-                        <CardContent>
-                            <Typography gutterBottom variant="h5" component="div">
-                                {d.title}
-                            </Typography>
-                            <Typography variant="body2" color="text.secondary">
-                                {d.content}
-                            </Typography>
-                        </CardContent>
-                        <CardActions>
-                            <Button size="small">Try</Button>
-                            <Button size="small">Learn More</Button>
-                        </CardActions>
-                    </Card>))
+                    messages.map(d => (
+                        <Card sx={{ my: 2 }}>
+                            <CardContent>
+                                {
+                                    d.author === 'user' ? (
+                                        <Typography gutterBottom variant="h5" component="div">
+                                            <Box sx={{
+                                                backgroundColor: '#888',
+                                                backgroundImage: `url(${user?.photoURL || ''})`,
+                                                backgroundSize: 'contain',
+                                                width: 28, height: 28,
+                                                borderRadius: '50%',
+                                                mr: 2,
+                                            }} />
+                                        </Typography>
+                                    ) : (<></>)
+                                }
+                                <Typography gutterBottom variant="h5" component="div">
+                                    {d.title}
+                                </Typography>
+                                <Typography variant="body2" color="text.secondary">
+                                    {d.content}
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small">Try</Button>
+                                <Button size="small">Learn More</Button>
+                            </CardActions>
+                        </Card>
+                    ))
                 }
             </Box>
             <PostButton onPostClicked={doPost} style={{ position: 'fixed', right: 24, bottom: 24 }} />
