928b21fb0ef6fdc9d223801f36be0bcf88931544
Koh Uehara <refluster@gmail.com>
2024-03-29 23:26:58 -0700

    refactor

--- src/page/index.tsx
+++ src/page/index.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Box, Typography, Button, Card, CardContent, CardActions } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import Header from '../component/header';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { auth } from '../lib/firebase';
