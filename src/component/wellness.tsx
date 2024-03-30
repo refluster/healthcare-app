10e1cb1acab583f824677e3ec25fade856309733
Koh Uehara <refluster@gmail.com>
2024-03-29 18:16:20 -0700

    refactor

---
+++ src/component/wellness.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react';
+import { Box } from '@mui/material';
+import { Wellness } from '../model';
+
+const WellnessWheel: React.FC<{ wellness: Wellness | undefined }> = ({ wellness }) => {
+    const colors: Record<string, string> = {
+        emotional: 'rgba(120, 111, 170, OPACITY)',    // emotionalの色
+        physical: 'rgba(170, 87, 153, OPACITY)',     // physicalの色
+        social: 'rgba(109, 175, 88, OPACITY)',      // socialの色
+        spiritual: 'rgba(241, 150, 91, OPACITY)',   // spiritualの色
+        financial: 'rgba(93, 181, 249, OPACITY)',    // financialの色
+        intellectual: 'rgba(231, 187, 76, OPACITY)' // intellectualの色
+    };
+
+    const angles: Record<string, number> = {
+        emotional: 0,
+        physical: 60,
+        social: 120,
+        spiritual: 180,
+        financial: 240,
+        intellectual: 300
+    };
+
+    if (wellness === undefined) {
+        return <Box sx={{ w: 200, h: 200 }}></Box>;
+    }
+
+    const gradArr = Object.entries(wellness).splice(0, 6).map(([key, value]) =>
+        `${angles[key]}deg, ${colors[key].replace('OPACITY', '0')} ${25 * (1 - value) + 45}%, ${colors[key].replace('OPACITY', '1')} ${45 * (1 - value) + 55}%`);
+    const gradient = gradArr.map(grad => `linear-gradient(${grad})`).join(',');
+    console.log(gradArr);
+
+    const wheelStyle = {
+        borderRadius: '50%',
+        backgroundImage: gradient,
+        backgroundBlendMode: 'hard-light',
+    };
+
+    return <>
+        <ul style={{ padding: 0 }}>
+            {Object.entries(wellness || {}).map(([key, value]) => (
+                <li key={key} style={{ listStyleType: 'none', margin: '8px 0' }}>
+                    {`${key}: ${value}`}
+                    <div style={{ background: '#eee', borderRadius: '4px', overflow: 'hidden' }}>
+                        <div style={{
+                            background: colors[key].replace('OPACITY', '1   '),
+                            width: `${value * 100}%`,
+                            height: '10px',
+                            borderRadius: '4px'
+                        }} />
+                    </div>
+                </li>
+            ))}
+        </ul>
+        <Box sx={{ width: 200, height: 200, mx: 'auto' }} style={wheelStyle}></Box>
+    </>;
+};
+
+export default WellnessWheel;
--- src/page/stats.tsx
+++ src/page/stats.tsx
@@ -9,6 +9,7 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { useNavigate } from 'react-router-dom';
 import { DailyStats, Wellness } from '../model';
 import * as api from '../api';
+import WellnessWheel from '../component/wellness';
 
 Chart.register(...registerables);
 //ChartJS.register(ArcElement, Tooltip, Legend);
@@ -52,61 +53,6 @@ const StatsPage: React.FC = () => {
             */}
         </Box>
     );
-};
-
-const WellnessWheel: React.FC<{ wellness: Wellness | undefined }> = ({ wellness }) => {
-    const colors: Record<string, string> = {
-        emotional: 'rgba(120, 111, 170, OPACITY)',    // emotionalの色
-        physical: 'rgba(170, 87, 153, OPACITY)',     // physicalの色
-        social: 'rgba(109, 175, 88, OPACITY)',      // socialの色
-        spiritual: 'rgba(241, 150, 91, OPACITY)',   // spiritualの色
-        financial: 'rgba(93, 181, 249, OPACITY)',    // financialの色
-        intellectual: 'rgba(231, 187, 76, OPACITY)' // intellectualの色
-    };
-
-    const angles: Record<string, number> = {
-        emotional: 0,
-        physical: 60,
-        social: 120,
-        spiritual: 180,
-        financial: 240,
-        intellectual: 300
-    };
-
-    if (wellness === undefined) {
-        return <Box sx={{ w: 200, h: 200 }}></Box>;
-    }
-
-    const gradArr = Object.entries(wellness).splice(0, 6).map(([key, value]) =>
-        `${angles[key]}deg, ${colors[key].replace('OPACITY', '0')} ${25 * (1 - value) + 45}%, ${colors[key].replace('OPACITY', '1')} ${45 * (1 - value) + 55}%`);
-    const gradient = gradArr.map(grad => `linear-gradient(${grad})`).join(',');
-    console.log(gradArr);
-
-    // インラインスタイルでウェルネスホイールを定義
-    const wheelStyle = {
-        borderRadius: '50%',
-        backgroundImage: gradient,
-        backgroundBlendMode: 'hard-light',
-    };
-
-    return <>
-        <ul style={{ padding: 0 }}>
-            {Object.entries(wellness || {}).map(([key, value]) => (
-                <li key={key} style={{ listStyleType: 'none', margin: '8px 0' }}>
-                    {`${key}: ${value}`}
-                    <div style={{ background: '#eee', borderRadius: '4px', overflow: 'hidden' }}>
-                        <div style={{
-                            background: colors[key].replace('OPACITY', '1   '),
-                            width: `${value * 100}%`,
-                            height: '10px',
-                            borderRadius: '4px'
-                        }} />
-                    </div>
-                </li>
-            ))}
-        </ul>
-        <Box sx={{ width: 200, height: 200, mx: 'auto' }} style={wheelStyle}></Box>
-    </>;
 };
 
 const BarChart = () => {
